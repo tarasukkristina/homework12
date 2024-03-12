61dc76566834c5e3b549321321f9d78d5f255749
Kristina Tarasuk <tarasukkristina92gmail.com>
2024-03-12 14:07:18 +0300

    solution

--- src/by/it/academy/homework12/HW12.java
+++ src/by/it/academy/homework12/HW12.java
@@ -1,4 +1,35 @@
 package by.it.academy.homework12;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class HW12 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        getAndPrintSquareOfNumbers();
+    }
+
+    public static void getAndPrintSquareOfNumbers() throws ExecutionException, InterruptedException {
+        List<Integer> listOfNumbers = new ArrayList<>();
+        listOfNumbers.add(7);
+        listOfNumbers.add(8);
+        listOfNumbers.add(5);
+        listOfNumbers.add(2);
+
+        List<CompletableFuture<Integer>> squareOfNumbersFutureList = new ArrayList<>();
+
+        for (int i = 0; i< listOfNumbers.size(); i++) {
+            int number = listOfNumbers.get(i);
+            CompletableFuture<Integer> squareOfNumbersFuture = CompletableFuture.supplyAsync(() -> number*number);
+            squareOfNumbersFutureList.add(squareOfNumbersFuture);
+        }
+
+        List<Integer> squareNumbers = new ArrayList<>();
+
+        for (CompletableFuture<Integer> future:squareOfNumbersFutureList){
+            squareNumbers.add(future.get());
+        }
+        System.out.println("Result square of numbers " + squareNumbers);
+    }
 }
